d0d91a39df93686a6eb9176c2b65a09ceef68877
Antoine CORNELLE <antoine.cornelle@outlook.fr>
2015-02-02 00:59:16 +0100

    ajout heroku

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -64,6 +64,8 @@ grails.project.dependency.resolution = {
         compile ":asset-pipeline:1.9.9"
 		compile ':spring-security-core:2.0-RC4'
 		compile ":glyph-icons:0.1.0"
+		compile ':heroku:1.0.1'
+		compile ':cloud-support:1.0.11'
 		
         // plugins needed at runtime but not for compilation
         runtime ":hibernate4:4.3.6.1" // or ":hibernate:3.6.10.18"
